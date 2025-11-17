4c22ff46aaecf300ecbdfd432249b52b15b31366
medProgAyat <hosseinmehriyari1383@gmail.com>
2025-11-17 12:48:09 +0330

    افزودن توابع ضرب و تقسیم

--- calculator.py
+++ calculator.py
@@ -4,5 +4,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "خطا: تقسیم بر صفر!"
+    return a / b
+
 if __name__ == "__main__":
     print("ماشین‌حساب ساده")
